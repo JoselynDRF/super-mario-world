4d3795044e3b6c90ff6cadb62b5a1e3a7240129b
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-08-11 19:32:08 +0100

    Create block

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Block : MonoBehaviour {
+  
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Mario") {
+			this.gameObject.layer = 8;
+		} else {
+			this.gameObject.layer = 13;
+		}
+	}
+
+}
